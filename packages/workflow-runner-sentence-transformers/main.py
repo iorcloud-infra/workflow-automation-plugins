9fd0b09df2e4c70d919edc140c830f2f6f19a4b8
Mayank Shinde <mayank31313@gmail.com>
2025-07-11 04:17:57 +0530

    update

--- packages/workflow-runner-sentence-transformers/main.py
+++ packages/workflow-runner-sentence-transformers/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="Workflow Runner Sentence Transformers",
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+)
 
 @app.get("/")
 async def root() -> dict:
